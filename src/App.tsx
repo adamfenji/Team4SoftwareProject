a67ec295a99d301c71d5cd2e1e37901521b31eac
Cami Maynard <cjmaynar@mtu.edu>
2023-10-16 12:00:48 -0400

    Deleted the test txt file and changed our title

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Testing</h1>
+      <h1>Activity Tracker</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
