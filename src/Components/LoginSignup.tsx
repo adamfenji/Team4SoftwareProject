984fc199cf5e343b47048b1f601a4df5cb851fd6
cbleemtu <cblee@mtu.edu>
2023-10-18 12:33:52 -0400

    Moved Sign Up and Logins

--- src/Components/LoginSignup.tsx
+++ src/Components/LoginSignup.tsx
@@ -10,10 +10,16 @@ const LoginSignup = () => {
 
   return (
     <div className = 'container'>
+      <div className = "submit-container">
+      <div className = {action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+      <div className = "or">or</div>
+      <div className = {action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+        </div>
       <div className = 'header'>
         <div className = "text">{action}</div>
         <div className = "underline"></div>
       </div>
+      
       <div className = "inputs">
         {action==="Login"?<div></div>:<div className="input">
         <img src={user_icon} alt=""/>
@@ -31,10 +37,7 @@ const LoginSignup = () => {
       </div>
       {action==="Sign Up"?<div></div>:<div className = "forgot-password">Forgot Password? <span>Click Here!</span></div>}
       
-      <div className = "submit-container">
-      <div className = {action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-      <div className = {action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
-        </div>
+      
     </div>
   )
 };
