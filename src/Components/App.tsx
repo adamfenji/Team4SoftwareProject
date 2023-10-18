01fa1fb3d00ea4ecf2e91484e72f0183f2b4cb32
Cami Maynard <cjmaynar@mtu.edu>
2023-10-17 20:14:48 -0400

    deleted some stuff

---
+++ src/Components/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LoginSignup from './LoginSignup';
+import './LoginSignup.css'
+function App() {
+  return (
+  <div>
+    <LoginSignup/>
+  </div>
+  );
+}
+
+export default App;
