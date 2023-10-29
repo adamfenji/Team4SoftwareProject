62084b2545beb9f0f2c492000922cea306a41513
jrbartos <147950302+jrbartos@users.noreply.github.com>
2023-10-29 15:58:20 -0400

    Fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import LoginSignup from './Components/LoginSignup.tsx';
-import './Components/LoginSignup.css'
+import LoginSignup from '../components/LoginSignup';
+import 'styles/LoginSignup.css'
 
 function App() {
   return (
