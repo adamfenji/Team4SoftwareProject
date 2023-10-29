deacb7b4e52be3d6a979da8ccf1a2480354f9389
cbleemtu <147186596+cbleemtu@users.noreply.github.com>
2023-10-29 18:52:33 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import '../styles/App.css'
 // import LoginSignup from '../components/LoginSignup';
 // import PhysicalTracker from "../components/PhysicalTracker.tsx";
 import '../styles/LoginSignup.css';
-import Dashboard from '../components/Dashboard/Dashboard';
+//import Dashboard from '../components/Dashboard/Dashboard';
 import LoginSignup from '../components/LoginSignup';
 function App() {
   return (    
