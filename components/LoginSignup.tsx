8b6db33b254268996935351cfaa6f6a60ef2dea9
Adam Fenjiro <afenjiro@mtu.edu>
2023-10-31 13:22:14 -0400

    Fixed CSS code, flex display on the login page

--- components/LoginSignup.tsx
+++ components/LoginSignup.tsx
@@ -1,57 +1,59 @@
 import { useState } from 'react';
 import '../styles/LoginSignup.css'
 // Does specific file address work in actual website?
-import user_icon from '../assets/person.png' 
+import user_icon from '../assets/person.png'
 import email_icon from '../assets/email.png'
 import password_icon from '../assets/password.png'
 import Button from "./Button"
 
 const LoginSignup = () => {
 
-  const [action,setAction] = useState("Sign Up");
+  const [action, setAction] = useState("Sign Up");
 
   return (
-    <div className = 'logincontainer'>
-      <div className = "submit-container">
-      <div className = {action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
-      <div className = "or">or</div>
-      <div className = {action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+    <div className="pageContainer">
+      <div className='logincontainer'>
+        <div className="submit-container">
+          <div className={action === "Login" ? "submit gray" : "submit"} onClick={() => { setAction("Sign Up") }}>Sign Up</div>
+          <div className="or">or</div>
+          <div className={action === "Sign Up" ? "submit gray" : "submit"} onClick={() => { setAction("Login") }}>Login</div>
         </div>
-      <div className = 'header'>
-        <div className = "text">{action}</div>
-        <div className = "underline"></div>
+        <div className='header'>
+          <div className="text">{action}</div>
+          <div className="underline"></div>
+        </div>
+
+        <div className="inputs">
+          {action === "Login" ? <div></div> : <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>}
+
+          <div className="input">
+            <img src={email_icon} alt="" />
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className="input">
+            <img src={password_icon} alt="" />
+            <input type="password" placeholder="Password" />
+          </div>
+        </div>
+        {action === "Sign Up" ? <div></div> : <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>}
+
+        <Button
+          margin="auto"
+          border="none"
+          backgroundColor="#4c00b4"
+          height="75px"
+          onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}
+          width="100px"
+          alignItems="center"
+          borderRadius="50px"
+          children="Confirm"
+          color="white"
+        />
+
       </div>
-      
-      <div className = "inputs">
-        {action==="Login"?<div></div>:<div className="input">
-        <img src={user_icon} alt=""/>
-        <input type="text" placeholder="Name"/>
-      </div>}
-      
-      <div className="input">
-        <img src={email_icon} alt=""/>
-        <input type="email" placeholder="Email"/>
-      </div>
-      <div className="input">
-        <img src={password_icon} alt=""/>
-        <input type="password" placeholder="Password"/>
-      </div>
-      </div>
-      {action==="Sign Up"?<div></div>:<div className = "forgot-password">Forgot Password? <span>Click Here!</span></div>}
-      
-      <Button
-      margin = "auto"
-      border = "none"
-      backgroundColor = "#4c00b4"
-      height = "75px"
-      onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}
-      width = "100px"
-      alignItems= "center"
-      borderRadius = "50px"
-      children = "Confirm"
-      color = "white"
-      />
-      
     </div>
   )
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import '../styles/App.css'
-// import LoginSignup from '../components/LoginSignup';
-// import PhysicalTracker from "../components/PhysicalTracker.tsx";
 import '../styles/LoginSignup.css';
-<<<<<<< HEAD
-import Dashboard from '../components/Dashboard/Dashboard';
-=======
-//import Dashboard from '../components/Dashboard/Dashboard';
->>>>>>> deacb7b4e52be3d6a979da8ccf1a2480354f9389
 import LoginSignup from '../components/LoginSignup';
 function App() {
   return (    
   <>
     <LoginSignup/> 
-    {/* <Dashboard/> */}
   </>
   );
 }
