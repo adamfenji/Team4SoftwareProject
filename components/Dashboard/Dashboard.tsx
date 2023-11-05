3ce0ddc02cf966d0333ffaa9686b6b92455170e6
Adam Fenjiro <afenjiro@mtu.edu>
2023-11-05 06:25:25 -0500

    Added animations +Added flipCard effect + fixed widget colors + Fixed sizes

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,20 +1,21 @@
 import SideBar from "./SideBar.tsx";
 import ActivityCalendar from "./ActivityCalendar.tsx";
 import WidgetGoals from "./WidgetGoals.tsx";
+import 'animate.css';
 
 import "./Dashboard.css";
 
 function Dashboard() {
     return (
         <div className="dashboardContainer">
-            <div className="sidebarDashboard">
+            <div className="sidebarDashboard animate__animated animate__slideInLeft animate__delay-1s">
                 <SideBar />
             </div>
             <div className="contentDashboard">
                 <ActivityCalendar />
                 <WidgetGoals/>
                 <div className="chartContainer">
-                    <h3>ChartContainer</h3>
+                    {/* <h3>ChartContainer</h3> */}
                 </div>
             </div>
 
--- components/Dashboard/SideBar.tsx
+++ components/Dashboard/SideBar.tsx
@@ -3,78 +3,153 @@ import { FiSettings } from "react-icons/fi";
 import { ImStatsDots } from "react-icons/im";
 import { BiLogOutCircle, BiBody } from "react-icons/bi";
 import { FaAppleAlt, FaBrain } from "react-icons/fa";
-import { CgProfile } from "react-icons/cg";
+import { CgMoreVertical, CgProfile } from "react-icons/cg";
 
 import "./SideBar.css";
 
+import { useState } from "react";
+
 function SideBar() {
+
+    const [expanded, setExpanded] = useState(true);
+
     return (
-        <aside className="sidebar">
+        <aside className="sidebar animate__animated animate__slideInLeft animate__delay-1s">
+            {expanded ? (
+                <>
+                    <CgMoreVertical
+                        className="actionButton"
+                        onClick={() => setExpanded(false)}
+                    />
 
-            {/* div containing the logo and name of the web application */}
-            <div className="logo">
-                <img src="..\assets\logo-sample.jpg" alt="logo" />
-                <h2>ActivityTracker</h2>
-            </div>
+                    {/* div containing the logo and name of the web application */}
+                    <div className="logo">
+                        <img src="..\assets\logo-sample.jpg" alt="logo" />
+                        <h2>ActivityTracker</h2>
+                    </div>
 
-            {/* ul containg the sections of the web application.
+                    {/* ul containg the sections of the web application.
                     -h4 are the main sections
                     -li are the tabs under that section
             */}
-            <ul className="links">
-                <h4>Main Menu</h4>
-                <li>
-                    <LuLayoutDashboard className="icon" 
-                    onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}>/
-                    </LuLayoutDashboard>
-                    <a href="#"
-                    onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}>Dashboard</a>
-                </li>
+                    <ul className="links">
+                        <h4>Main Menu</h4>
+                        <li>
+                            <LuLayoutDashboard className="icon"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}>/
+                            </LuLayoutDashboard>
+                            <a href="#"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}>Dashboard</a>
+                        </li>
 
-                <h4>Trackers</h4>
-                <li>
-                    <BiBody className="icon" />
-                    <a href="#">Physical Tracker</a>
-                </li>
-                <li>
-                    <FaAppleAlt className="icon" />
-                    <a href="#">Dietary Tracker</a>
-                </li>
-                <li>
-                    <FaBrain className="icon" />
-                    <a href="#">Mental Tracker</a>
-                </li>
+                        <h4>Trackers</h4>
+                        <li>
+                            <BiBody className="icon" />
+                            <a href="#">Physical Tracker</a>
+                        </li>
+                        <li>
+                            <FaAppleAlt className="icon" />
+                            <a href="#">Dietary Tracker</a>
+                        </li>
+                        <li>
+                            <FaBrain className="icon" />
+                            <a href="#">Mental Tracker</a>
+                        </li>
 
-                <h4>Suggestions</h4>
-                <li>
-                    <ImStatsDots className="icon" />
-                    <a href="#">Analytics</a>
-                </li>
+                        <h4>Suggestions</h4>
+                        <li>
+                            <ImStatsDots className="icon" />
+                            <a href="#">Analytics</a>
+                        </li>
 
-                <h4>Account</h4>
-                <li>
-                    <CgProfile className="icon" 
-                    onClick={() => window.location.href = "/Team4SoftwareProject/profile"}>/
-                    </CgProfile>
-                    <a href="#"
-                    onClick={() => window.location.href = "/Team4SoftwareProject/profile"}>Profile
-                    </a>
-                </li>
-                <li>
-                    <FiSettings className="icon" />
-                    <a href="#">Settings</a>
-                </li>
-                <h4>Account</h4>
-                <li>        
-                    <BiLogOutCircle className="icon" 
-                    onClick={() => window.location.href = "/Team4SoftwareProject"}>/
-                    </BiLogOutCircle>
-                    <a href="#"
-                     onClick={() => window.location.href = "/Team4SoftwareProject"}>Logout
-                    </a>
-                </li>
-            </ul>
-            
+                        <h4>Account</h4>
+                        <li>
+                            <CgProfile className="icon"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/profile"}>/
+                            </CgProfile>
+                            <a href="#"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/profile"}>Profile
+                            </a>
+                        </li>
+                        <li>
+                            <FiSettings className="icon" />
+                            <a href="#">Settings</a>
+                        </li>
+                        <h4>Account</h4>
+                        <li>
+                            <BiLogOutCircle className="icon"
+                                onClick={() => window.location.href = "/Team4SoftwareProject"}>/
+                            </BiLogOutCircle>
+                            <a href="#"
+                                onClick={() => window.location.href = "/Team4SoftwareProject"}>Logout
+                            </a>
+                        </li>
+                    </ul>
+                </>
+            ) : (
+                <>
+                    <CgMoreVertical
+                        className="actionButton"
+                        onClick={() => setExpanded(true)}
+                    /><br />
+
+                    {/* div containing the logo and name of the web application */}
+                    <div className="logo-extanded">
+                        <img src="..\assets\logo-sample.jpg" alt="logo" />
+                    </div>
+
+                    {/* ul containg the sections of the web application.
+                    -h4 are the main sections
+                    -li are the tabs under that section
+            */}
+                    <ul className="links-extanded">
+                        <li>
+                            <a href="#"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}>
+                                    <LuLayoutDashboard className="icon"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/dashboard"}>/
+                            </LuLayoutDashboard>
+                                </a>
+                        </li>
+                        <li>
+
+                            <a href="#"><BiBody className="icon" /></a>
+                        </li>
+                        <li>
+
+                            <a href="#"><FaAppleAlt className="icon" /></a>
+                        </li>
+                        <li>
+
+                            <a href="#"><FaBrain className="icon" /></a>
+                        </li>
+                        <li>
+
+                            <a href="#"><ImStatsDots className="icon" /></a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/profile"}>
+                                    <CgProfile className="icon"
+                                onClick={() => window.location.href = "/Team4SoftwareProject/profile"}>
+                            </CgProfile>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#"><FiSettings className="icon" /></a>
+                        </li>
+                        <li>
+                            <a href="#"
+                                onClick={() => window.location.href = "/Team4SoftwareProject"}>
+                                    <BiLogOutCircle className="icon"
+                                onClick={() => window.location.href = "/Team4SoftwareProject"}>
+                            </BiLogOutCircle>
+                            </a>
+                        </li>
+                    </ul>
+
+                </>
+            )}
         </aside>
     );
 }
