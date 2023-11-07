ef3441a5d192160c881e33904ac6da203fac79cc
cbleemtu <cblee@mtu.edu>
2023-11-06 20:00:17 -0500

    Fixing Last Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import '../styles/App.css'
 import '../styles/LoginSignup.css';
 import LoginSignup from '../components/LoginSignup';
-import React from 'react';
+//import React from 'react';
 
 function App() {
   return (    
