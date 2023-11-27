cb3b24186c7385d62843e43e118d46357d2273eb
jrbartos <147950302+jrbartos@users.noreply.github.com>
2023-11-27 18:50:40 -0500

    Physical Tracker functionality

--- Front-end/components/PhysicalTracker/PhysicalTracker.tsx
+++ Front-end/components/PhysicalTracker/PhysicalTracker.tsx
@@ -9,6 +9,7 @@ interface WorkoutGoal {
 const PhysicalTracker: FC<{}> = () => {
   const [workoutGoals, setWorkoutGoals] = useState<string>('');
   const [selectedWorkout, setSelectedWorkout] = useState<string | null>(null);
+  const [selectedBodyPart, setSelectedBodyPart] = useState<string | null>(null);
 
   const handleGoalChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setWorkoutGoals(event.target.value);
@@ -26,8 +27,18 @@ const PhysicalTracker: FC<{}> = () => {
 
   const handleWorkoutSelection = (workoutType: string) => {
     setSelectedWorkout(workoutType);
-    // You can navigate to the website associated with the workout type here
-    // Example: window.location.href = `https://example.com/${workoutType}`;
+    // Reset body part selection when a new workout type is selected
+    setSelectedBodyPart(null);
+  };
+
+  const handleBodyPartSelection = (bodyPart: string) => {
+    setSelectedBodyPart(bodyPart);
+    // Fetch exercises based on the selected body part (you can implement this logic)
+    // Example: axios.get(`http://localhost:3000/api/exercises?bodyPart=${bodyPart}`)
+    //   .then(response => {
+    //     // Handle the response and update the UI with exercises
+    //   })
+    //   .catch(error => console.error('Error fetching exercises:', error));
   };
 
   return (
@@ -50,20 +61,58 @@ const PhysicalTracker: FC<{}> = () => {
         </form>
       </div>
 
+      <br />
+
       <div className='workouts'>
         <h2>Workouts</h2>
-        <div>
-          <p>Select a workout type:</p>
-          <button onClick={() => handleWorkoutSelection('Aerobic')}>Aerobic</button>
-          <button onClick={() => handleWorkoutSelection('Pilates')}>Pilates</button>
-          <button onClick={() => handleWorkoutSelection('Yoga')}>Yoga</button>
-          <button onClick={() => handleWorkoutSelection('pilates')}>Pilates</button>
-          {/* Add more buttons for other workout types */}
+        <div className="WorkoutsType">
+          <div>
+            <div className='workoutTitle'>
+              <h3>Select a workout type:</h3>
+            </div>
+            <div className='workoutTypeButtons'>
+              <button onClick={() => handleWorkoutSelection('Pilates')}>Pilates</button>
+              <button onClick={() => handleWorkoutSelection('Yoga')}>Yoga</button>
+              <button onClick={() => handleWorkoutSelection('Mobility')}>Mobility</button>
+              <button onClick={() => handleWorkoutSelection('Stretch')}>Stretch</button>
+              <button onClick={() => handleWorkoutSelection('Dance')}>Dance</button>
+
+              <button onClick={() => handleWorkoutSelection('Strength')}>Strength</button>
+              <button onClick={() => handleWorkoutSelection('Cardio')}>Cardio</button>
+              <button onClick={() => handleWorkoutSelection('HIIT')}>HIIT</button>
+              <button onClick={() => handleWorkoutSelection('Running')}>Running</button>
+              <button onClick={() => handleWorkoutSelection('No-Equipment')}>No-Equipment</button>
+
+              <button onClick={() => handleWorkoutSelection('Self-Defense')}>Self-Defense</button>
+
+            </div>
+          </div>
+
+          {selectedWorkout === 'Strength' && (
+            <div className='strengthBodyPartSelection'>
+              <h3>Select a specific area for Strength training:</h3>
+              <div className='bodyPartButtons'>
+                <button onClick={() => {
+                  handleBodyPartSelection('Upper Body');
+                  window.location.href = 'https://www.youtube.com/watch?v=XxuRSjER3Qk';
+                }}>
+                  Full Body
+                </button>
+                <button onClick={() => handleBodyPartSelection('Lower Body')}>Lower Body</button>
+
+              </div>
+
+              {selectedBodyPart && (
+                <div className='selectedBodyPart'>
+                  <p>Selected Body Part: {selectedBodyPart}</p>
+                </div>
+              )}
+            </div>
+          )}
         </div>
         {selectedWorkout && (
-          <div>
+          <div className='selectedWorkout'>
             <p>Selected Workout: {selectedWorkout}</p>
-            {/* You can add additional content or link to a website here */}
           </div>
         )}
       </div>
